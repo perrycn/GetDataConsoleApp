fa25ff9b883aec630979502228dc9d6450a378be
perrycn <perrycn@charter.net>
2017-09-22 13:34:04 -0500

    GetDataConsoleApp for Hello World check-in

---
+++ GetDataConsoleApp/GetDataConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace GetDataConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetrieveDataSync strSync = new RetrieveDataSync();
+            string strOutput = strSync.GetDataFromAPI();
+
+            AppSettingsReader ar = new AppSettingsReader();
+            string outLocation = (string)ar.GetValue("dataoutputlocation", typeof(string));
+            if (outLocation == "Console") Console.WriteLine(strOutput);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GetDataConsoleApp/GetDataConsoleApp/RetrieveDataSync.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace GetDataConsoleApp
+{
+    public class RetrieveDataSync
+    {
+        public string GetDataFromAPI()
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Content-Type:application/json");
+                client.Headers.Add("Accept:application/json");
+                var result = client.DownloadString("http://localhost:49589/api/Data");
+                return result;
+            }
+        }
+    }
+}
